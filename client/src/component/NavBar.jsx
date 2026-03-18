c957679073a509d99caf8e2ad17f3e6e4b1b4335
sinsin13 <adsnh13gmail.com>
2026-03-19 01:25:34 +0530

    updated some backend

--- client/src/component/NavBar.jsx
+++ client/src/component/NavBar.jsx
@@ -2,11 +2,11 @@
 import React from "react";
 const NavBar = () => {
     return (
-        <div className="w-full flex justify-between h-15 bg-gray-200 shadow px-5 items-center">    
+        <div className="w-full flex justify-between h-15 bg-pink-100 shadow px-5 items-center">    
 
 
             <div className="w-[10%] flex items-center h-full">
-                <h1 className="text-zinc-800 font-bold">Logo</h1>
+                <h1 className="text-zinc-800 font-bold">SNH</h1>
             </div>
 
 
